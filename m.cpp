773f9c49b03edfbec8bb2f836cbdbe45fe263af6
Sayleyd <yastreb4848@gmail.com>
2023-04-02 21:18:04 +0300

    the fib(n) function has been added to the program, which returns the nth Fibonacci number, provided that the series starts with 1

--- m.cpp
+++ m.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
-
 using namespace std;
+
+int fib(int n) {
+	if (n <= 0) return -1;
+	if (n <= 2) return 1;
+	return fib(n - 1) + fib(n - 2);
+}
 
 int main() {
 	cout << "Hello, World!";
