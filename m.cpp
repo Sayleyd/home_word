becf24850e956eb06c4d9166dbd990001174df23
Sayleyd <yastreb4848@gmail.com>
2023-04-02 22:05:01 +0300

    now the fib(n) function returns nothing, and the values of the Fibonacci series are calculated iteratively and output to standard output

--- m.cpp
+++ m.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
+#define forn(i, n) for(int i = 0; i < int(n); ++i)
+
 using namespace std;
 
-int fib(int n) {
-	if (n <= 0) return -1;
-	if (n <= 2) return n - 1;
-	return fib(n - 1) + fib(n - 2);
+
+void fib(int n) {
+	int a = 0, b = 1;
+	forn(i, n) {
+		cout << a << '\n';
+		b = a + b, a = b - a;
+	}
 }
+
 
 int main() {
 	cout << "Hello, World!";
